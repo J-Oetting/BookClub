d5c0cf06cac520636486b2416f690689a92696cb
J-Oetting <joetting71@gmail.com>
2021-12-13 17:50:18 -0500

    created command handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Intents, Interaction, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -9,5 +10,30 @@ const client  = new Client({ intents: [Intents.FLAGS.GUILDS]});
 client.once('ready', () => {
     console.log('Ready!');
 })
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error){
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephermal: true})
+    }
+
+    
+});
 
 client.login(token);
