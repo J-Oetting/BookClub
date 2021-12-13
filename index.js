70eb41e80c84a6918202c23d714e434960bcc2aa
J-Oetting <joetting71@gmail.com>
2021-12-13 13:10:43 -0500

    new client created

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const token = process.env.DISCORD_TOKEN;
+const client  = new Client({ intents: [Intents.FLAGS.GUILDS]});
+
+client.once('ready', () => {
+    console.log('Ready!');
+})
+
+client.login(token);
