c0c715b84abb4b16fe0b8033c4d30d69540d40af
Jordan Oetting <JOetting71@gmail.com>
2021-12-28 16:11:16 -0500

    uncommented index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const fs = require('fs');
+const fs = require('fs');
 const { Client, Intents, Interaction, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 
@@ -11,29 +11,29 @@ client.once('ready', () => {
     console.log('Ready!');
 })
 
-// client.commands = new Collection();
-// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// for (const file of commandFiles){
-//     const command = require(`./commands/${file}`);
-//     client.commands.set(command.data.name, command);
-// }
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
-// client.on('interactionCreate', async interaction => {
-//     if (!interaction.isCommand()) return;
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
-//     const command = client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-//     if (!command) return;
+    if (!command) return;
 
-//     try {
-//         await command.execute(interaction);
-//     } catch (error){
-//         console.error(error);
-//         await interaction.reply({ content: 'There was an error while executing this command!', ephermal: true})
-//     }
+    try {
+        await command.execute(interaction);
+    } catch (error){
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephermal: true})
+    }
 
     
-// });
+});
 
 client.login(token);
