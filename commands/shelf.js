4ff0b3f40332eb3d9537ca7b20d6f07ff84a4f74
Jordan Oetting <JOetting71@gmail.com>
2022-01-05 14:57:03 -0500

    built out shelf, but need to retool for through table

--- commands/shelf.js
+++ commands/shelf.js
@@ -1,43 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { db, Models } = require('../database/index');
-const { currentBook } = require('./current')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('set-current')
-    .setDescription(`Use this to set what we're currently reading.`)
+    .setName('add-to-shelf')
+    .setDescription('Add a book you want to read later to the club shelf. Only 5 books can be on the shelf at a given time.')
     .addStringOption( option => option.setName('title').setDescription(`title`))
     .addStringOption( option => option.setName('author').setDescription('author'))
     .addStringOption( option => option.setName('isbn').setDescription('ISBN')),
-    async execute (interaction){
-        
+    async execute(interaction){
+
         const title = interaction.options.getString('title');
         const author = interaction.options.getString('author');
         const isbn = interaction.options.getString('isbn');
         const search = title.replace(/\s/, '').toLowerCase();
-        const oldRead = await currentBook(interaction.member.guild.id);
 
-
-        if ( !title || !author){
+        if (!title || !author){
             await interaction.reply(`You need the ${ title ? `author` : `title`} to add a book`);
             return;
         }
-
         let book ;
 
         if (isbn){
-            book = await Models.Book.findOrCreate({
+
+            [book, created]= await Models.Book.findOrCreate({
                 where: {
                     isbn: isbn
                 },
                 defaults: {
                     title: title,
                     author: author,
-                    search: search
+                    search: search,
                 }
             })
         } else {
-            book = await Models.Book.findOrCreate({
+
+            [book, created] = await Models.Book.findOrCreate({
                 where: {
                     search: search
                 },
@@ -48,27 +46,9 @@ module.exports = {
             })
         }
 
-        if (book[0].current){
-            interaction.reply(`We're alreay reading this, but thanks for contributing!`);
-            return;
-        }
+        
 
-        
-        const club = await Models.Club.findOne({
-            where: {
-                guildId: interaction.member.guild.id
-            }
-        })
-        
-        if (oldRead){
-            await club.addBook(oldRead, { through: { current: false}});
-        }
-        
-        console.log('old', oldRead);
-        
-        await club.addBook(book[0], { through: { current: true }});
-
-        await interaction.reply(`Great! The club is now reading: ${title} by ${author}`)
-
+        //book exists, now add it to the waitlist
+        // what is the waitlist?
     }
 }
